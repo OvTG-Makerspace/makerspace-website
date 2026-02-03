aa9e1c365d3357efa9f5a4dc439824ef4e3807b9
simonkdev <simonkorten09@gmail.com>
2026-02-03 10:12:41 +0100

    finish environment

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+  };
+
+  packages = [
+    pkgs.nodejs
+  ];
+
+  enterShell = ''
+    if [ ! -f package.json ]; then
+      npm init -y
+    fi
+
+    if [ ! -d node_modules ]; then
+      npm install \
+        express \
+        express-handlebars \
+        nodemailer \
+        nodemailer-express-handlebars
+    fi
+  '';
+}
